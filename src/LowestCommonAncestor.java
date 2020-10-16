96a0e3768359e81a1cdbbcc3dcced59131097c4c
12jmanning <12jmanning@gmail.com>
2020-10-16 12:44:54 +0100

    Adding Extra Testing

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -98,6 +98,8 @@ public class LowestCommonAncestor
         System.out.println("LCA(4, 6): " + tree.findLCA(4,6));
         System.out.println("LCA(3, 4): " + tree.findLCA(3,4));
         System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
+        System.out.println("LCA(5, 6): " + tree.findLCA(5,6));
+
 
     }
 }
